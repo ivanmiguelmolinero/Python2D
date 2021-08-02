c166835b6ec603ae3ec960ba5d19de4607207374
ivanmiguelmolinero <i.miguel.molinero@gmail.com>
2021-08-02 17:25:19 +0200

    Movimiento esfera

---
+++ Capitulo1/ejemplo_movimiento_esfera.py
@@ -0,0 +1,49 @@
+#-------------------------------------------------------------------------------
+# Name:        ejemplo_movimiento_esfera
+# Purpose:
+#
+# Author:      ivijo
+#
+# Created:     02/08/2021
+# Copyright:   (c) ivijo 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import cocos
+from pyglet import image
+from pyglet.window import key
+from collections import defaultdict
+
+class Actor(cocos.sprite.Sprite):
+    def __init__(self, imagen, x, y):
+        super().__init__(image.load(imagen))
+        self.position = (x, y)
+
+class MiCapa(cocos.layer.Layer):
+    is_event_handler = True
+    def __init__(self):
+        super().__init__()
+        self.mi_actor = Actor('./Capitulo1/mi_esfera.png', 320, 240)
+        self.add(self.mi_actor)
+        self.velocidad = 100.0
+        self.teclas_pulsadas = defaultdict(int)
+        self.schedule(self.update)
+
+    def on_key_press(self, k, m):
+        self.teclas_pulsadas[k] = 1
+    def on_key_release(self, k, m):
+        self.teclas_pulsadas[k] = 0
+    
+    def update(self, dt):
+        x = self.teclas_pulsadas[key.RIGHT] - self.teclas_pulsadas[key.LEFT]
+        y = self.teclas_pulsadas[key.UP] - self.teclas_pulsadas[key.DOWN]
+        if x != 0 or y!= 0:
+            posicion = self.mi_actor.position
+            nueva_x = posicion[0] + self.velocidad * x * dt * 2
+            nueva_y = posicion[1] + self.velocidad * y * dt * 2
+            self.mi_actor.position = (nueva_x, nueva_y)
+
+if __name__ == '__main__':
+    cocos.director.director.init(caption = 'Movimiento de esfera mediante teclado')
+    mi_escena = cocos.scene.Scene(MiCapa())
+    cocos.director.director.run(mi_escena)
